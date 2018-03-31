10162a552513272cf9a131b12e090b3b7dffcb38
Alex <alex@alexseifert.com>
2018-04-01 01:50:34 +0200

    Spacing changes

--- ScratchPad/AppDelegate.swift
+++ ScratchPad/AppDelegate.swift
@@ -10,9 +10,6 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         let note = Note(name: "new name here", content: "some sort of content")
@@ -25,7 +22,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
