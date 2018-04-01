363959f8c12bc47354ce49225fb34d54dc6276e0
Alex <alex@alexseifert.com>
2018-04-01 22:16:17 +0200

    Added Toggle Editor Toolbar menu item

--- ScratchPad/View-Window/TextViewController.swift
+++ ScratchPad/View-Window/TextViewController.swift
@@ -10,10 +10,16 @@ import Cocoa
 
 class TextViewController: NSViewController {
     @IBOutlet var mainTextView: NSTextView!
+    @IBOutlet weak var noteTitleField: NSTextField!
+    @IBOutlet weak var noteDateLabel: NSTextField!
+    @IBOutlet weak var notePageNumberLabel: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        noteTitleField.stringValue = "title"
+        noteDateLabel.stringValue = "date"
+        notePageNumberLabel.stringValue = "0"
     }
 
     override var representedObject: Any? {
@@ -22,5 +28,8 @@ class TextViewController: NSViewController {
         }
     }
     
+    @IBAction func toggleEditorToolbar(_ sender: AnyObject) {
+        mainTextView.usesInspectorBar = mainTextView.usesInspectorBar ? false : true
+    }
 }
 
