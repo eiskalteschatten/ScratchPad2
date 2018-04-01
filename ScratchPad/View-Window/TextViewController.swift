96173184e882e4c7f7c55d20c0fae52e7f5a6f00
Alex <alex@alexseifert.com>
2018-04-01 02:08:17 +0200

    Removed the view controller for the primary text view

--- ScratchPad/View-Window/TextViewController.swift
+++ ScratchPad/View-Window/TextViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class TextViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
